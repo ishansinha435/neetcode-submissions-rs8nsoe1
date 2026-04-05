8db5634ce3594e66b4b7e88fefc6ff406fc8419b
ishansinha435 <ishan_sinha@live.com>
2026-04-05 04:24:45 -0400

    Add: foreign-dictionary - submission-0

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adj = {c : set() for w in words for c in w}
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            min_len = min(len(w1), len(w2))
+            for j in range(min_len):
+                if w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
+            if w1[:min_len] == w2[:min_len] and len(w1) > len(w2):
+                return ""
+
+        res = []
+        visited = {}
+        def dfs(c):
+            if c in visited:
+                return visited[c]
+            visited[c] = True
+            for nei in adj[c]:
+                if dfs(nei):
+                    return True
+            visited[c] = False
+            res.append(c)
+        
+        for c in adj:
+            if dfs(c):
+                return ""
+        res.reverse()
+        return "".join(res)
